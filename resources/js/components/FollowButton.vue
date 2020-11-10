55d2c2c2dab6ba219cf434f514da10b2ea3c23dc
Khin Me Me Latt <khinmemelatt@outlook.com>
2020-11-10 19:33:13 +0630

    Many To Many Relationship

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-        <button class="btn btn-primary ml-4" @click="followUser">Follow</button>
+        <button class="btn btn-primary ml-4" @click="followUser" v-text="buttonText"></button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['userId'],
+        props: ['userId', 'follows'],
+
+        data: function(){
+            return {
+                status: this.follows
+            }
+        },
 
         mounted() {
             console.log('Component mounted.')
@@ -16,8 +22,20 @@
             followUser() {
                 axios.post('/follow/'+this.userId)
                     .then(response => {
-                        alert(response.data);
+                        this.status = ! this.status;
+                        console.log(response.data);
+                    })
+                    .catch(errors => {
+                        if(errors.response.status == 401) {//401 is unauthorized error
+                            window.location = '/login'
+                        }
                     });
+            }
+        },
+
+        computed: {
+            buttonText() {
+                return (this.status) ? 'Unfollow' : 'Follow';
             }
         }
     }
